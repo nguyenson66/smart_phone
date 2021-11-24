1132449b6be204867026849dd1cbb2046d38f822
nguyenvu272 <nguyen.vu.27022001@gmail.com>
2021-11-24 09:52:09 +0700

    update logo

--- src/index.js
+++ src/index.js
@@ -1,59 +1,61 @@
-const express = require('express')
-const routes = require('./routes')
-const exphb = require('express-handlebars')
-const cookieParser = require('cookie-parser')
-const path = require('path')
+const express = require("express");
+const routes = require("./routes");
+const exphb = require("express-handlebars");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded( {extended : true}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 // set express-handlebars
-app.engine('hbs',exphb({
-    extname : '.hbs',
-    helpers : {
-        mul : (a,b) => {
-            a *= b
-            a  += ''
-            let mn = a.split('').reverse(), count = 0
-            res = ''
-            for (let i = 0;i<mn.length;i++){
-                count++
-                res += mn[i]
-                if(count%3==0 && i != mn.length-1)
-                    res += '.'
-            }
-            return res.split('').reverse().join('')
+app.engine(
+    "hbs",
+    exphb({
+        extname: ".hbs",
+        helpers: {
+            mul: (a, b) => {
+                a *= b;
+                a += "";
+                let mn = a.split("").reverse(),
+                    count = 0;
+                res = "";
+                for (let i = 0; i < mn.length; i++) {
+                    count++;
+                    res += mn[i];
+                    if (count % 3 == 0 && i != mn.length - 1) res += ".";
+                }
+                return res.split("").reverse().join("");
+            },
+            sum: (a, b) => a + b,
+            vnd: (a) => {
+                a += "";
+                let mn = a.split("").reverse(),
+                    count = 0;
+                res = "";
+                for (let i = 0; i < mn.length; i++) {
+                    count++;
+                    res += mn[i];
+                    if (count % 3 == 0 && i != mn.length - 1) res += ".";
+                }
+                return res.split("").reverse().join("");
+            },
         },
-        sum : (a,b) => a+b,
-        vnd : (a) => {
-            a += ''
-            let mn = a.split('').reverse(), count = 0
-            res = ''
-            for (let i = 0;i<mn.length;i++){
-                count++
-                res += mn[i]
-                if(count%3==0 && i != mn.length-1)
-                    res += '.'
-            }
-            return res.split('').reverse().join('')
-        }
-    }
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname + '/views'))
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname + "/views"));
 
 // set view engine Pug
 // app.set('view engine', 'pug')
 // app.set('views',path.join(__dirname+'/views'))
 
-
 ////// All routes web ////////
-routes(app)
+routes(app);
 
 app.listen(3000, () => {
-    console.log('server is running')
-})
+    console.log("server is running at http://localhost:3000");
+});
